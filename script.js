63aa6292acf41b6545e5081c5f449804d2b389e3
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-16 13:16:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer"
+      ], function (Map, MapView, FeatureLayer) {
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "topo-vector"
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
+          zoom: 6,
+          center: [-90, 38]
         });
+
+
+        /********************
+         * Add feature layer
+         ********************/
+
+        // COVID-19 cases per 100,000 throughout the St. Louis.
+        var featureLayer_1 = new FeatureLayer({
+  url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/STL_Neighborhood/FeatureServer"
+});
+
+map.add(featureLayer_1);
+
+var featureLayer_2 = new FeatureLayer({
+  url: "https://services8.arcgis.com/judPKWn4qwnKEdZl/arcgis/rest/services/COVID19_Case_Rate_Per_100k_in_STL/FeatureServer"
+});
+
+map.add(featureLayer_2);
+
+        map.add(featureLayer);
       });
