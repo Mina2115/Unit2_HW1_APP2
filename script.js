a75475480ba56ed8f5ab76314790d5e84d2b4b98
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-16 20:15:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ require([
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 13,
+          zoom: 20,
           center: [-90, 38]
         });
 
